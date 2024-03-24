2427ed61e6c51819611d8edce371a1635b9e1fdb
Matt Rollender <matt@rollender.com>
2024-03-24 14:06:17 -0400

    minecraft

--- lib/mrr/gaming.nix
+++ lib/mrr/gaming.nix
@@ -2,7 +2,7 @@
   imports = [ ./licensing.nix ];
 
   config = {
-    home.packages = [ pkgs.gtkcord4 pkgs.steam pkgs.r2modman ];
+    home.packages = [ pkgs.gtkcord4 pkgs.steam pkgs.r2modman pkgs.minecraft ];
 
     mrr.licensing.allowed_unfree_pkg_names = [
       "steam"
