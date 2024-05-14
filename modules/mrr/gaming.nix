41f58d92e91e15c05b2b1880cd62fdc491d5237a
Matt Rollender <matt@rollender.com>
2024-05-14 19:21:45 -0400

    remove minecraft since its broken

--- modules/mrr/gaming.nix
+++ modules/mrr/gaming.nix
@@ -2,13 +2,12 @@
   imports = [ ./licensing.nix ];
 
   config = {
-    home.packages = [ pkgs.gtkcord4 pkgs.steam pkgs.r2modman pkgs.minecraft ];
+    home.packages = [ pkgs.gtkcord4 pkgs.steam pkgs.r2modman ];
 
     mrr.licensing.allowed_unfree_pkg_names = [
       "steam"
       "steam-original"
       "discord"
-      "minecraft-launcher"
     ]; # using gtkcord should allow me to remove discord from this
   };
 }
