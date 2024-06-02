635f309c7a58f2253b685b3345be9453b60a7311
Matt Rollender <matt@rollender.com>
2024-06-02 16:35:31 -0400

    newsboat colors

--- modules/mrr/newsboat.nix
+++ modules/mrr/newsboat.nix
@@ -2,18 +2,25 @@
 {
   programs.newsboat = {
     enable = true;
-    urls = {
-      "somerville-times" = {
+    urls = [
+      {
         url = "https://www.thesomervilletimes.com/feed";
         tags = [
           "news"
           "local"
           "somerville"
         ];
-      };
-    };
+      }
+    ];
+
     autoReload = true;
     browser = "brave"; # TODO: parameterize this
     reloadTime = 60;
+
+    # TODO parameterize colors to i3 colors if possible
+    extraConfig = ''
+      color info green black
+      color listfocus_unread green black bold
+    '';
   };
 }
