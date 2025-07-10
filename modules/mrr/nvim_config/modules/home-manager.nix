0f792bdbe163dc8105982d1a3aa10f4b5587d436
Matt Rollender <matt@rollender.com>
2025-07-10 14:24:28 -0400

    Revert "Revert "Insert nvim config""

---
+++ modules/mrr/nvim_config/modules/home-manager.nix
@@ -0,0 +1,78 @@
+{
+  rose-pine,
+  nil,
+  conform,
+  nickel-lang-lsp,
+  vim-nickel,
+  ...
+}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  vimPlugins = pkgs.vimPlugins.extend (
+    final': prev': {
+      rose-pine = pkgs.vimUtils.buildVimPlugin {
+        name = "rose-pine";
+        src = rose-pine;
+      };
+      mrr-config = pkgs.vimUtils.buildVimPlugin {
+        name = "mrr-config";
+        src = ../src;
+      };
+      conform = pkgs.vimUtils.buildVimPlugin {
+        name = "conform";
+        src = conform;
+      };
+      vim-nickel = pkgs.vimUtils.buildVimPlugin {
+        name = "vim-nickel";
+        src = vim-nickel;
+      };
+    }
+  );
+  lsps = [
+    pkgs.lua-language-server
+    pkgs.haskellPackages.haskell-language-server
+    nil
+    pkgs.elixir-ls
+    pkgs.nixfmt-rfc-style
+    pkgs.stylua
+    nickel-lang-lsp
+  ];
+  custom-neovim = {
+    enable = true;
+    extraLuaConfig = ''
+      NVIM_CONFIG_ELIXIR_LS_PATH = "${pkgs.elixir-ls}/lib/language_server.sh"
+      require("mrr");
+    '';
+    plugins = [
+      vimPlugins.telescope-nvim
+      vimPlugins.nvim-lspconfig
+      vimPlugins.nvim-cmp
+      vimPlugins.diffview-nvim
+      vimPlugins.rose-pine
+      vimPlugins.conform
+      vimPlugins.nvim-treesitter
+      vimPlugins.mrr-config
+      vimPlugins.vim-tmux-navigator
+      vimPlugins.formatter-nvim
+      vimPlugins.vim-nickel
+      vimPlugins.vim-sexp
+      vimPlugins.vim-sexp-mappings-for-regular-people
+      vimPlugins.nvim-surround
+      vimPlugins.conjure
+      vimPlugins.cmp-conjure
+      # todo: add the cmp conjure for nrepl
+      (vimPlugins.nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
+    ];
+  };
+in
+{
+  config = {
+    programs.neovim = custom-neovim;
+    home.packages = lsps;
+  };
+}
